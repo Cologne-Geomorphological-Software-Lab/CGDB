07d7fddec34a678c2316102a111352164c67e4d5
Dennis Handy <106592296+d-handy@users.noreply.github.com>
2025-12-09 16:26:32 +0100

    Update settings.py

--- prototype/settings.py
+++ prototype/settings.py
@@ -64,8 +64,6 @@ INSTALLED_APPS = [
     "import_export",
     "django_filters",
     "crispy_forms",
-    "zero_migrations",
-    "sphinxcontrib_django",
     # CGDB Apps
     "prototype",  
     "field_data",  
