e19a634881686c830938f94f94b33e0d56fb55b2
Dennis Handy <106592296+d-handy@users.noreply.github.com>
2026-02-25 16:36:19 +0100

    added src

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../../src"))
+sys.path.insert(0, os.path.abspath("/src"))
 
 project = "CGDB"
 copyright = "2026, Dennis Handy, W. Marijn Van der Meij"
