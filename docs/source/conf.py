ff9d44046a890cd5eaf1b1e1106ae074faf1f6e3
Dennis Handy <106592296+d-handy@users.noreply.github.com>
2026-02-25 17:11:20 +0100

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@ import os
 import sys
 import django
 
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath(os.path.join("..", "..", "src")))
 os.environ["DJANGO_SETTINGS_MODULE"] = "prototype.settings"
 django.setup()
 
