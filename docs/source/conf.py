e3123267f3813511eeb1f04da35d7d98dd5b1b9c
Dennis Handy <106592296+d-handy@users.noreply.github.com>
2026-02-25 16:58:04 +0100

    modified conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,7 @@ import sys
 import django
 
 sys.path.insert(0, os.path.abspath(".."))
-os.environ["DJANGO_SETTINGS_MODULE"] = "Your_project_name.settings"
+os.environ["DJANGO_SETTINGS_MODULE"] = "prototype.settings"
 django.setup()
 
 project = "CGDB"
